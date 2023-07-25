24d85cdf7d92c5a6e8f66402fc5e772979887459
bseufw <ChandlerX101@gmail.com>
2023-07-25 15:40:12 -0400

    update nav
    added dropdown menu

--- ivey-sus/src/App.js
+++ ivey-sus/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Routes, Route, Link } from 'react-router-dom';
+import { Routes, Route} from 'react-router-dom';
 import Layout from './pages/Layout';
 import Home from './pages/Home';
 import Events from './pages/Events';
