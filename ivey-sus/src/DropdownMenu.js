dec68538bf16538ebe682e97020fdccc536ae314
bseufw <ChandlerX101@gmail.com>
2023-08-15 21:59:03 -0400

    added poo dropdown
    skull

---
+++ ivey-sus/src/DropdownMenu.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const DropdownMenu= () => {
+    return(
+        <div className="dropdown-menu">
+            <ul>
+                <button>Upcoming</button>
+                <button>Past</button>
+            </ul>
+        </div>
+    );
+};
+
+export default DropdownMenu;
--- ivey-sus/src/Header.js
+++ ivey-sus/src/Header.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Home from './pages/Home';
 import { Routes, Route, Link } from 'react-router-dom';
 import Events from './pages/Events';
@@ -10,12 +10,24 @@ import Toolbar from "@mui/material/Toolbar";
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
 import AdbIcon from '@mui/icons-material/Adb';
+import DropdownMenu from "./DropdownMenu"
 
 
 
 
 
 export default function Header(){
+
+  const [isDropdownVisible, setDropDownVisible] = useState(false);
+
+  const handleMouseEnter = () => {
+    setDropDownVisible(true);
+  };
+
+  const handleMouseLeave = () => {
+    setDropDownVisible(false);
+  };
+
     return(
         <AppBar style={{ backgroundColor: 'green' }} position="static">
         <Toolbar>
@@ -36,7 +48,25 @@ export default function Header(){
             Ivey Sustainability
           </Typography>
           <Button component={Link} to="/" color="inherit">Home</Button>
-          <Button component={Link} to="/events" color="inherit">Events</Button>
+
+
+          <div onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave} style={{ position: 'relative' }}>
+            <Button color="inherit">Events</Button>
+            {isDropdownVisible && (
+            <div
+              style={{
+                position: 'absolute', // Set absolute positioning
+                top: '100%', // Position below the Events button
+                left: 0, // Align with the Events button
+                backgroundColor: 'white', // Add background color for the dropdown
+                boxShadow: '0 0 5px rgba(0, 0, 0, 0.2)', // Add a box shadow for styling
+              }}
+            >
+              <DropdownMenu />
+            </div>
+          )}
+        </div>
+
           <Button component={Link} to="/ourteam" color="inherit">Our Team</Button>
           <Button component={Link} to="/media" color="inherit">Media</Button>
           <Button component={Link} to="/contact" color="inherit">Contact</Button>
