2934517371dbedcfc409dc64333cf3dde6fa939e
pranav <pranavsworkemail@gmail.com>
2023-08-17 14:50:21 -0400

    implemented past/upcoming

--- ivey-sus/src/pages/Events.js
+++ ivey-sus/src/pages/Events.js
@@ -14,9 +14,9 @@ const Events = () => {
             <Stripe title="Events" description="A showcase of upcoming events."></Stripe>
             <div>
                 {/* 400 character limit? Just so we don't have scaling issues */}
-                <EventCard imgFile="testimage3.jpg" title="Title" date="June 2, 2023" description={text}></EventCard>
-                <EventCard imgFile="testimage3.jpg" title="Title" date="June 2, 2023" description={text}></EventCard>
-                <EventCard imgFile="testimage3.jpg" title="Title" date="June 2, 2023" description="hellloooooooo description description description description description description description "></EventCard>
+                <EventCard imgFile="testimage3.jpg" title="Title" date="June 2, 2024" description={text}></EventCard>
+                <EventCard imgFile="testimage3.jpg" title="Title" date="June 2, 2025" description={text}></EventCard>
+                <EventCard imgFile="testimage3.jpg" title="Title" date="June 2, 2026" description="hellloooooooo description description description description description description description "></EventCard>
             </div>
         </div>
     )
