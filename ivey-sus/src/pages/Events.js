99baa5e966713c3ac49729a1f11db67a24a681f5
pranav <pranavsworkemail@gmail.com>
2023-08-17 14:48:51 -0400

    basically finished events page(not both)

--- ivey-sus/src/pages/Events.js
+++ ivey-sus/src/pages/Events.js
@@ -1,16 +1,22 @@
 import React from 'react';
 import Stripe from '../Stripe';
+import EventCard from '../EventCard';
 
 
 const Events = () => {
+
+    var text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum"
     return (
         <div>
-            <div style={{height: '10px', backgroundColor: 'blue'}}>
+            <div style={{height: '10px', backgroundColor: 'white'}}>
             {/* LINE BREAK  */}
             </div>
             <Stripe title="Events" description="A showcase of upcoming events."></Stripe>
             <div>
-                
+                {/* 400 character limit? Just so we don't have scaling issues */}
+                <EventCard imgFile="testimage3.jpg" title="Title" date="June 2, 2023" description={text}></EventCard>
+                <EventCard imgFile="testimage3.jpg" title="Title" date="June 2, 2023" description={text}></EventCard>
+                <EventCard imgFile="testimage3.jpg" title="Title" date="June 2, 2023" description="hellloooooooo description description description description description description description "></EventCard>
             </div>
         </div>
     )
