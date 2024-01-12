c3250f22e1de930598050fc479837e72e32f814a
chandlerkx <88215414+chandlerkx@users.noreply.github.com>
2024-01-11 20:26:14 -0500

    Update HomeBanner.js

--- ivey-sus/src/components/HomeBanner.js
+++ ivey-sus/src/components/HomeBanner.js
@@ -4,7 +4,7 @@ export default function HomeBanner() {
     return (
     <div style={{position:'relative', width:'100%',height:'auto'}}>
             <img style={{ width: '100%', height: '100%'}} src="/images/iveybuilding.png" alt="Ivey Building"/>
-            <p style={{position:'absolute', top:'20%', left:'5%',color:'#125900', fontSize:'5rem', maxWidth:'40%'}}>Ivey MSc Sustainability Club</p>
+            <p style={{position:'absolute', top:'20%', left:'5%',color:'#125900', fontSize:'4rem', maxWidth:'40%'}}>Ivey MSc Sustainability Club</p>
     </div>
 );
 }
