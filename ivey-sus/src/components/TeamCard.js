a01aab84fcc3c46c7868fcf44b5332227892e824
chandlerkx <88215414+chandlerkx@users.noreply.github.com>
2024-01-11 22:17:45 -0500

    Update TeamCard.js

--- ivey-sus/src/components/TeamCard.js
+++ ivey-sus/src/components/TeamCard.js
@@ -5,23 +5,30 @@ export default function TeamCard({ imgFile, name, role, emailLink, linkedinLink,
     const [isHovered, setIsHovered] = useState(false);
 
     return (
-        <div
-            onMouseEnter={() => setIsHovered(true)}
-            onMouseLeave={() => setIsHovered(false)}
-        >
-            <div style={{paddingRight:'3rem'}}>
-                <img src={imgFile} alt={name} style={{width:'100%', height:'auto', filter: isHovered ? 'blur(5px)' : 'none', transition: 'filter 1s'}}></img>
+        <div>
+            <div
+                onMouseEnter={() => setIsHovered(true)}
+                onMouseLeave={() => setIsHovered(false)}
+            >
+                <div style={{paddingRight:'3rem'}}>
+                    <img src={imgFile} alt={name} style={{width:'100%', height:'auto', filter: isHovered ? 'blur(5px)' : 'none', transition: 'filter 0.1s'}}></img>
+                </div>
             </div>
-            <div style={{}}>
-                <p style={{ fontWeight: 'bold', fontSize: '2rem', fontFamily: 'Arial', color: '#104702', paddingTop: '10%' }}>{name}</p>
-                <p style={{ fontWeight: '700', lineHeight: '0', fontSize: '1.6rem', fontFamily: 'Arial', color: '#374A1D', paddingTop: '9%' }}>{role}</p>
-                <a href={emailLink} target="_blank" rel="noopener noreferrer">
-                    <img style={{ paddingTop: '10%' }} src='/icons/mail.png' alt="Email"/>
-                </a>
-                <a style={{ marginLeft: "3%" }} href={linkedinLink} target="_blank" rel="noopener noreferrer">
-                    <img src='/icons/linkedin.png' alt="LinkedIn"/>
-                </a>
-            </div>
+                <div style={{}}>
+                    <p style={{ fontWeight: 'bold', fontSize: '2rem', fontFamily: 'Arial', color: '#104702', paddingTop: '10%' }}>{name}</p>
+                    <p style={{ fontWeight: '700', lineHeight: '0', fontSize: '1.6rem', fontFamily: 'Arial', color: '#374A1D', paddingTop: '9%' }}>{role}</p>
+                    
+                </div>
+                
+            
+            <div>
+            <a href={emailLink} target="_blank" rel="noopener noreferrer">
+                <img style={{ paddingTop: '10%' }} src='/icons/mail.png' alt="Email"/>
+            </a>
+            <a style={{ marginLeft: "3%" }} href={linkedinLink} target="_blank" rel="noopener noreferrer">
+                <img src='/icons/linkedin.png' alt="LinkedIn"/>
+            </a>
         </div>
+    </div>
     );
 }
